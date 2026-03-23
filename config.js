33323af9bbd9f73bfca95fc019afda1e99746172
kessahoud-art <kessahoud@gmail.com>
2026-03-23 12:51:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 
 const FLIVO_CONFIG = {
   supabase_url:      'REMPLACE_PAR_TON_URL',       // ex: https://xxxx.supabase.co
-  supabase_anon_key: 'REMPLACE_PAR_TA_ANON_KEY',   // ex: eyJhbGc...
+  supabase_anon_key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtdGZ6ZGNkbWtlYWZnbWhkcnppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyNjM3MTIsImV4cCI6MjA4OTgzOTcxMn0.BpRSvoE75tSQRy_v3WoEpnTy9clu7qXyckUrqr5Aec8',   // ex: eyJhbGc...
   app_name:          'Flivo',
   app_url:           'https://flivo.vercel.app',    // ton domaine Vercel
 }
